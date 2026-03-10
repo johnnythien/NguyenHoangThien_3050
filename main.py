94dd6147d94542df344da1fb1000c8959f178398
nguyen hoang thien-3050 <nguyenhoanthien472004@gmail.com>
2026-03-10 10:07:30 +0700

    sum and subtract a , b

---
+++ main.py
@@ -0,0 +1,14 @@
+# Tính tổng và hiệu của hai số
+
+def sum(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+
+    print("Sum of a and b:", sum(a, b))
+    print("Subtract of a and b:", subtract(a, b))
